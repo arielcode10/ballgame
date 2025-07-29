4e70a902d6c50a2578cbe23d525a5852cd834f75
arielcode10 <arieliahaiat@gmail.com>
2025-07-30 00:27:15 +0300

    add-food

--- game.cpp
+++ game.cpp
@@ -1,26 +1,36 @@
 #include "raylib.h"
+#include <random>
+#include <time.h>
 
 // WINDOW size
 const int screenWidth = 800;
 const int screenHeight = 450;
 
+#define FOOD_BALL_SIZE 15.0f
+#define NEG_RATE 8
+
 void borderPlayer(Vector2 *ballPos, float ballRad, float ballSpd);
+void foodEat(bool *eaten, Vector2 *randPos, Vector2 ballPos, float *ballRad);
 
 int main()
 {
+    srand(time(NULL));
     InitWindow(screenWidth, screenHeight, "Raylib Example - Moving Ball");
 
     // Set the ball's initial position
     Vector2 ballPosition = {static_cast<float>(screenWidth) / 2, static_cast<float>(screenHeight) / 2};
+    Vector2 randPos = {0, 0};
     float ballRadius = 20.0f;
     float ballSpeed = 200.0f;
+    bool foodEaten = false;
 
     SetTargetFPS(60); // Set desired frame rate
 
     // Main game loop
     while (!WindowShouldClose())
     {
-        // Update
+        ballRadius += 0.1;
+        // Move
         if (IsKeyDown(KEY_RIGHT))
             ballPosition.x += ballSpeed * GetFrameTime();
         if (IsKeyDown(KEY_LEFT))
@@ -31,6 +41,7 @@ int main()
             ballPosition.y -= ballSpeed * GetFrameTime();
 
         borderPlayer(&ballPosition, ballRadius, ballSpeed);
+        foodEat(&foodEaten, &randPos, ballPosition, &ballRadius);
 
         // Draw
         BeginDrawing();
@@ -49,7 +60,7 @@ int main()
 
 /***********/
 // Function adds border logic into window.
-// INPUT: ballPos - the position of the ball, 
+// INPUT: ballPos - the position of the ball,
 // ballRad - the radius of the ball,
 // ballSpd - the speed of the ball.
 // OUTPUT: None
@@ -58,7 +69,7 @@ void borderPlayer(Vector2 *ballPos, float ballRad, float ballSpd)
 {
     if (ballPos->y >= screenHeight - ballRad) // if hit bottom
     {
-        ballPos->y -= ballSpd * GetFrameTime(); 
+        ballPos->y -= ballSpd * GetFrameTime();
     }
     if (ballPos->x >= screenWidth - ballRad) // if hit right side
     {
@@ -72,4 +83,31 @@ void borderPlayer(Vector2 *ballPos, float ballRad, float ballSpd)
     {
         ballPos->x += ballSpd * GetFrameTime();
     }
+}
+
+/***********/
+// Function adds food and eating to make ball smaller.
+// INPUT: ballPos - the position of the ball,
+// eaten - check if food was eaten
+// ballRad - the radius of the ball,
+// randPos - the random position of the food.
+// OUTPUT: None
+/***********/
+void foodEat(bool *eaten, Vector2 *randPos, Vector2 ballPos, float *ballRad)
+{
+    if (!*eaten) // if food eaten
+    {
+        // get random pos
+        randPos->x = static_cast<float>(rand() % screenWidth);
+        randPos->y = static_cast<float>(rand() % screenHeight);
+        *eaten = true;
+    }
+
+    if (CheckCollisionPointCircle(*randPos, ballPos, *ballRad)) // if eating food
+    {
+        *eaten = false;
+        (*ballRad) -= NEG_RATE; // grow smaller
+    }
+    // draw food onto screen
+    DrawCircleV(*randPos, FOOD_BALL_SIZE, GREEN);
 }
